b2ecb36e7168fcd83aaa07a76e806b18a4c5c2bd
Warren <deed821@gmail.com>
2014-05-19 11:05:18 +1000

    slimkat patch

---
+++ patch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR=$(pwd)
+
+#cd $DIR/build/
+#git fetch https://github.com/deedwar/platform_build.git beanstalk && git cherry-pick 0109de6907cda2d2a89f7b1773b0671fd2a7b439
+
+#cd $DIR/build/
+#git fetch https://github.com/deedwar/platform_build.git beanstalk && git cherry-pick 5798cb28fcfd809e7c68b79f0ded2f6c65d32e98
+
+cd $DIR/frameworks/native/
+git fetch https://github.com/deedwar/android_frameworks_native.git beanstalk-kitkat && git cherry-pick a893b04d6f52aa48d4207cf8e52a36f6f40114c6
+
+cd $DIR/frameworks/native/
+git fetch https://github.com/deedwar/android_frameworks_native.git beanstalk-kitkat && git cherry-pick 00d1de13f0d370f7853ab0a060181da4504f5af4
+
